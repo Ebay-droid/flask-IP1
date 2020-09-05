1fb92a17f7c4dec12b5829b35d74466314283785
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-09-05 21:14:36 +0300

    def main fnction initiated

--- run.py
+++ run.py
@@ -10,5 +10,44 @@ def create_user(user_name, password):
     new_user = User(user_name,password)
     return new_user
   
+def save_user(user):  
+    '''
+    save user
+    '''
+    user.save_user()
+  
 def create_credentials(account,user_id,pass_code):
+    '''
+    save user credentials
+    '''  
+
+    new_credentials =Credentials(account,user_id,pass_code)
+    return new_credentials
+  
+def save_credentials(credentials):
+    '''
+    save credentials
+    '''
+    credentials.save_credentials()
     
+def delete_credentials(credentials):
+  '''
+  to delete your credentials
+  '''
+  credentials.delete_credentials()    
+  
+def display_credentials():
+  '''
+  returns all saved credentials
+  '''
+  Credentials.display_credentials
+  
+def main():
+  print ("Hello and welcome to Password Locker. ")
+        
+  
+    
+      
+  
+  
+  
